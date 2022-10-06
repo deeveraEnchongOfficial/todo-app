a148dd421942a84ea49008877092e7fc525bff87
deeveraenchongofficial <deevera.enchong18@gmail.com>
2022-10-06 10:16:47 +0800

    first commit

---
+++ src/redux/states.js
@@ -0,0 +1,14 @@
+export let todos = [
+    {
+        id:1,
+        name: "Sample1",
+    },
+    {
+        id:2,
+        name: "Sample2",
+    },
+    {
+        id:3,
+        name: "Sample3",
+    },
+]
